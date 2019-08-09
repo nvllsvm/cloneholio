ac56835b13e9d61a9f328565229604328f58eaf6
Andrew Rabert <ar@nullsum.net>
2019-08-08 21:29:02 -0400

    Release version 0.7.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 0.7.0 (2019-08-08)
+- Add option to list remote repos then exit
+- Ignore error when argument GitHub repository is not found
+- Refactor into package
+- Remove `consumers` dependency
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='cloneholio',
-    version='0.6.1',
+    version='0.7.0',
     description='I am cloneholio! I need syncing of my repos.',
     license='MIT',
     author='Andrew Rabert',
